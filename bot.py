9008fb6d7140d67ac31e90642b5d99f3afd06446
Maxim-Pekov <max.pekov@gmail.com>
2023-03-21 17:22:25 +0300

    Show one random question in TG.

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import logging
 import os
+import random
+
 from dotenv import load_dotenv
 from textwrap import dedent
+from main import fetch_random_questions
 
 from telegram import ReplyKeyboardMarkup
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 
-
-# Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
@@ -38,9 +39,23 @@ def start(update: Update, context: CallbackContext) -> None:
                                      reply_markup=markup)
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def ask_question(update: Update, context: CallbackContext) -> None:
+
+    quiz_question = fetch_random_questions()
+    question = quiz_question[0].replace('.', '\.')
+    question = question.replace('-', '\-')
+    print(quiz_question)
+    greetings = dedent(f'Внимание вопрос:{question}')
+
+    message_keyboard = [["Новый вопрос ❔", "Сдаться ❌"],
+                        ['Мой счет ✍️']]
+    markup = ReplyKeyboardMarkup(
+        message_keyboard,
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+    update.message.reply_markdown_v2(text=greetings,
+                                     reply_markup=markup)
 
 
 def main() -> None:
@@ -50,8 +65,9 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
+    # dispatcher.add_handler(CommandHandler("Новый вопрос ", ask_question))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, ask_question))
 
     updater.start_polling()
     updater.idle()
--- main.py
+++ main.py
@@ -1,9 +1,60 @@
-from pprint import pprint
-with open("quiz-questions/1vs1200.txt", "r", encoding="KOI8-R") as my_file:
-    file_contents = my_file.read()
-f = file_contents.split('\n\n')
-slovar = {}
-for key, i in enumerate(f):
-    if 'Вопрос' in i:
-        slovar[f[key]] = f[key + 1]
-pprint(slovar)
+import os
+import random
+from collections import OrderedDict
+
+
+questions_path = []
+
+
+def shuffle_dict(dictionary):
+    keys = list(dictionary.keys())
+    random.shuffle(keys)
+    return OrderedDict([(key, dictionary[key]) for key in keys])
+
+
+def fetch_random_question_path():
+    if not questions_path:
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        QUESTIONS_DIR = os.path.join(BASE_DIR, 'quiz-bot', 'quiz-questions')
+
+        for root, dirs, files in os.walk(QUESTIONS_DIR):
+            for filename in files:
+                questions_path.append(os.path.join(root, filename))
+    return questions_path.pop()
+
+
+quiz_questions = OrderedDict()
+shuffle_questions = {}
+random_questions = {}
+
+
+def fetch_random_questions():
+    global random_questions
+    if random_questions:
+        return random_questions.popitem()
+    if not quiz_questions:
+        question_path = fetch_random_question_path()
+        with open(question_path, "r", encoding="KOI8-R") as my_file:
+            file_contents = my_file.read()
+        quiz_questionss = file_contents.split('\n\n')
+        for key, quiz_question in enumerate(quiz_questionss):
+            if 'Вопрос' in quiz_question:
+                question = quiz_questionss[key]
+                try:
+                    answer = quiz_questionss[key + 1]
+                except IndexError:
+                    continue
+                quiz_questions[question] = answer
+    if quiz_questions:
+        global shuffle_questions
+        if not shuffle_questions:
+            shuffle_questions = shuffle_dict(quiz_questions)
+        print(f'quiz_ques_count ->> {len(shuffle_questions)}')
+        five_random_questions = shuffle_questions.popitem()
+        print(f'quiz_ques ->> {five_random_questions}')
+        questions = five_random_questions[0].split('\n   ')[1:]
+        answers = five_random_questions[1].split('\n   ')[1:]
+        for key, random_question in enumerate(questions):
+            random_questions[random_question] = answers[key]
+
+    return random_questions.popitem()
