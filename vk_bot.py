882decd5b1c77a638d878de639dc84e0ba1abdeb
Maxim-Pekov <max.pekov@gmail.com>
2023-03-26 00:03:25 +0300

    Add keyboard to bot message.

--- vk_bot.py
+++ vk_bot.py
@@ -2,12 +2,24 @@ import random, os
 from dotenv import load_dotenv
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 
 def echo(event, vk_api):
+    keyboard = VkKeyboard(one_time=True)
+
+    # keyboard.add_button('Белая кнопка', color=VkKeyboardColor.DEFAULT)
+    keyboard.add_button('Зелёная кнопка', color=VkKeyboardColor.POSITIVE)
+
+    keyboard.add_line()  # Переход на вторую строку
+    keyboard.add_button('Красная кнопка', color=VkKeyboardColor.NEGATIVE)
+
+    keyboard.add_line()
+    keyboard.add_button('Синяя кнопка', color=VkKeyboardColor.PRIMARY)
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
+        keyboard=keyboard.get_keyboard(),
         random_id=random.randint(1,1000)
     )
 
